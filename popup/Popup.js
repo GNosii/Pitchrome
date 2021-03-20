9bb9d9cbe6abbe5283927738d0731f87105b1d4b
EuGNosii <65878291+GNosii@users.noreply.github.com>
2021-03-20 20:51:38 -0300

    Improvements to the error handling UI, and preventing other webs

--- popup/Popup.js
+++ popup/Popup.js
@@ -1,123 +1,136 @@
 // Script por ElNosii
 // Script manager de la interfaz gráfica de la extensión
 
-var ytThumbnailBaseUrl = "https://img.youtube.com/vi/$u/mqdefault.jpg";
+var ytThumbnailBaseUrl = 'https://img.youtube.com/vi/$u/mqdefault.jpg';
 var ytTitleReplaceRegex = /\(\d[\d]?[+]?\)/;
 
-var isYoutube = true;
+var isYoutube;
 var isError = false;
 var isFirefox = false;
 
 var isBrowserIdentified = false;
 
 var isShowingDebug = false;
+var isDebugBuild = true;
 
-setupUI();
 checkYoutube();
 listenToMouseClick();
 
 function setupUI(exception) {
     chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-        var videoId = (tab[0].url).replace("https://www.youtube.com/watch?v=", "").split("&")[0];
-        var videoTitle = (tab[0].title).replace(ytTitleReplaceRegex, "").replace(" - YouTube", "");
+        var videoId = (tab[0].url).replace('https://www.youtube.com/watch?v=', '').split('&')[0];
+        var videoTitle = (tab[0].title).replace(ytTitleReplaceRegex, '').replace(' - YouTube', '');
 
-    
         if (!isError) {
-            document.getElementById("btn-up").textContent = chrome.i18n.getMessage("buttonup");
-            document.getElementById("btn-down").textContent = chrome.i18n.getMessage("buttondown");
-            document.getElementById("btn-reset").textContent = chrome.i18n.getMessage("buttonreset");
+            document.getElementById('btn-up').textContent = chrome.i18n.getMessage('buttonup');
+            document.getElementById('btn-down').textContent = chrome.i18n.getMessage('buttondown');
+            document.getElementById('btn-reset').textContent = chrome.i18n.getMessage('buttonreset');
             
             if (isYoutube) {
-                document.getElementById("bkg-container").style.backgroundImage = "url('" + ytThumbnailBaseUrl.replace("$u", videoId); + "')";
-                document.getElementById("yt-title").textContent = videoTitle;
+                document.getElementById('bkg-container').style.backgroundImage = 'url(' + ytThumbnailBaseUrl.replace('$u', videoId); + ')';
+                document.getElementById('yt-title').textContent = videoTitle;
             }
         } else {
-            document.getElementById("main-container").style = "display: none;";
-            document.getElementById("bkg-container").style = "display: none;";
-            document.getElementById("error-container").style = "";
+            document.getElementById('main-container').style = 'display: none;';
+            document.getElementById('bkg-container').style = 'display: none;';
+            document.getElementById('error-container').style = '';
         
-            document.getElementById("error-title").textContent = chrome.i18n.getMessage("errortitle");
-            document.getElementById("error-desc").textContent = chrome.i18n.getMessage("errordesc");
-            document.getElementById("error-subtitle").textContent = chrome.i18n.getMessage("errorsubtitle");
-            document.getElementById("error-msg").textContent = exception.name + ": " + exception.message;
+            document.getElementById('error-title').textContent = chrome.i18n.getMessage('errortitle');
+            document.getElementById('error-desc').textContent = chrome.i18n.getMessage('errordesc');
+            document.getElementById('error-subtitle').textContent = chrome.i18n.getMessage('errorsubtitle');
+            document.getElementById('error-msg').textContent = "Report this on GitHub if it continues! " + exception.name + ': ' + exception.message;
+
+            return;
         }
 
-        // Añadir texto a la pantalla de depuración
-        document.getElementById("debug-title").textContent = "Información de depuración";
-        document.getElementById("debug-isfirefox").textContent = "Modo Firefox: " + isFirefox;
-        document.getElementById("debug-iserror").textContent = "Modo manejo de error: " + isError;
-        document.getElementById("debug-useragent").textContent = "Agente de Usuario: " + navigator.userAgent;
-        document.getElementById("debug-version").textContent = "Versión: " + chrome.runtime.getManifest().version;
+        if (isDebugBuild) {
+            document.getElementById('debug-title').textContent = 'Información de depuración';
+            document.getElementById('debug-isfirefox').textContent = 'Modo Firefox: ' + isFirefox;
+            document.getElementById('debug-iserror').textContent = 'Modo manejo de error: ' + isError;
+            document.getElementById('debug-useragent').textContent = 'Agente de Usuario: ' + navigator.userAgent;
+            document.getElementById('debug-version').textContent = 'Versión: ' + chrome.runtime.getManifest().version;
+            document.getElementById('debug-ytid').textContent = 'YTID: ' + videoId;
+            document.getElementById('debug-thumb').textContent = 'URL Thumbnail: ' + ytThumbnailBaseUrl.replace('$u', videoId);
+            document.getElementById('debug-ytunfilteredt').textContent = 'Título no filtrado: ' + tab[0].title;
 
+            document.getElementById('btn-debug').disabled = false
+        }
         // Añadirle texto al botón para evitar el problema de que si no se ha
         // seleccionado una opción no se ve el texto
-        document.getElementById("btn-debug").textContent = chrome.i18n.getMessage("buttondebugshow");
+        document.getElementById('btn-debug').textContent = chrome.i18n.getMessage('buttondebugshow');
 
-        console.log("Locale and images ready!");
+        console.log('Locale and images ready!');
     });
 }
 
 function listenToMouseClick() {
-    console.log("Attempting event register for " + navigator.userAgent);
+    console.log('Attempting event register for ' + navigator.userAgent);
 
-    if (navigator.userAgent.indexOf("Firefox")) {
-        document.addEventListener("mouseup", (event) => { handleClickEvent(event); });
-        console.log("Registered mouseup event listener");
+    if (navigator.userAgent.indexOf('Firefox') != -1) {
+        document.addEventListener('mouseup', (event) => { handleClickEvent(event); });
+        console.log('Registered mouseup event listener');
         isBrowserIdentified = true;
         isFirefox = true;
     }
 
-    if (navigator.userAgent.indexOf("Chrome") && !isBrowserIdentified) {
-        document.addEventListener("pointerdown", (event) => { handleClickEvent(event); });
-        console.log("Registered pointerdown event listener");
+    if (navigator.userAgent.indexOf('Chrome') && !isBrowserIdentified) {
+        document.addEventListener('pointerdown', (event) => { handleClickEvent(event); });
+        console.log('Registered pointerdown event listener');
         isBrowserIdentified = true;
     }
 
-    console.log("IsFirefox " + isFirefox);
+    console.log('IsFirefox ' + isFirefox);
 
     function handleClickEvent(event) {
         var clickedId = event.target.id 
 
-        if (clickedId == "btn-up" || clickedId == "btn-down" || clickedId == "btn-reset") {
-            console.log("Clicked on " + clickedId);
+        if (clickedId == 'btn-up' || clickedId == 'btn-down' || clickedId == 'btn-reset') {
+            console.log('Clicked on ' + clickedId);
 
             chrome.tabs.query({ active: true, currentWindow: true }, sendMessage);
         }
 
-        if (clickedId == "btn-debug") {
-            console.log("Clicked on debug button");
+        if (clickedId == 'btn-debug') {
+            console.log('Clicked on debug button');
 
             if (!isShowingDebug) {
-                document.getElementById("main-container").style = "display: none;";
-                document.getElementById("debug-container").style = "";
+                document.getElementById('main-container').style = 'display: none;';
+                document.getElementById('debug-container').style = '';
 
-                document.getElementById("btn-debug").textContent = chrome.i18n.getMessage("buttondebughide");
+                document.getElementById('btn-debug').textContent = chrome.i18n.getMessage('buttondebughide');
 
                 isShowingDebug = true;
             } else {
-                document.getElementById("main-container").style = "";
-                document.getElementById("debug-container").style = "display: none;";
+                document.getElementById('main-container').style = '';
+                document.getElementById('debug-container').style = 'display: none;';
 
-                document.getElementById("btn-debug").textContent = chrome.i18n.getMessage("buttondebugshow");
+                document.getElementById('btn-debug').textContent = chrome.i18n.getMessage('buttondebugshow');
 
                 isShowingDebug = false;
             }
         }
 
         function sendMessage(tab) {
-            var filtered = (event.target.id).replace("btn-", "");
+            var filtered = (event.target.id).replace('btn-', '');
             chrome.tabs.sendMessage(tab[0].id, {action: filtered});
 
-            console.log("Sent message to background script: " + filtered);
+            console.log('Sent message to background script: ' + filtered);
         }
 
-        console.debug("Received click event from " + event.type);
+        console.debug('Received click event from ' + event.type);
     }
 }
 
 function checkYoutube() {
-    // TODO: Chequear de alguna manera si es youtube (url o titulo de pestaña)
-    isYoutube = true;
+    chrome.tabs.query({ active: true, currentWindow: true}, (tab) => {
+        if ((tab[0].url).indexOf('www.youtube.com/') != -1) {
+            isYoutube = true;
+            setupUI();
+        } else {
+            isYoutube = false;
+            reportException(new Error(chrome.i18n.getMessage("errornoyoutube")));
+        }
+    });
 }
 
 function reportException(exception) {
